4a65463d684b59a176a7c6751087be2635cc83c0
LukyanetsDmitry <nikzimka@gmail.com>
2019-10-14 23:18:57 +0300

    Child Actor Components

--- Source/O5_TestingGrounds/O5_TestingGroundsHUD.cpp
+++ Source/O5_TestingGrounds/O5_TestingGroundsHUD.cpp
@@ -10,7 +10,7 @@
 AO5_TestingGroundsHUD::AO5_TestingGroundsHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
