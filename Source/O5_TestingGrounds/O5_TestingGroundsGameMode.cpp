45b0125ff14d33a84f762ab9f094e1973a10dcbf
LukyanetsDmitry <nikzimka@gmail.com>
2019-11-06 19:59:17 +0300

    Hit vs Overlap Events

--- Source/O5_TestingGrounds/O5_TestingGroundsGameMode.cpp
+++ Source/O5_TestingGrounds/O5_TestingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AO5_TestingGroundsGameMode::AO5_TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviuour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("Game/Dynamic/Character/Behaviour/Ceracter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
